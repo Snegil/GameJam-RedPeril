bf41967e9c70451b5d0d02a87d55287af195259d
Matildoz <matilda.jerreling@adelfors.nu>
2025-05-19 14:39:47 +0200

    grästextur och timer

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    int Score;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour
+{
+    int timmar;
+    int minuter;
+    float sekunder;
+    float millisekunder;
+    
+    [SerializeField] TextMeshProUGUI timerText;
+   
+    void Start()
+    {
+        sekunder = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sekunder += Time.deltaTime;
+        
+        if(sekunder > 60)
+        {
+            minuter++;
+            if(minuter >= 60)
+            {
+                minuter = 0;
+                timmar++;
+            }
+            sekunder = 0;
+        }    
+        timerText.text = (timmar + (minuter + (":") +Mathf.RoundToInt(sekunder)).ToString());
+    }
+}
