a1036bbae4a73f5133eaf0f35e1b85928a110e8b
Matildoz <matilda.jerreling@adelfors.nu>
2025-05-20 14:15:06 +0200

    Bana och highscore

---
+++ Assets/Scripts/Collectibles/HighScore.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour
+{
+    float highScore;
+    float score;
+    public Timer timer;
+    [SerializeField] TextMeshProUGUI highscore;
+    private void Start()
+    {       
+        highscore.text = PlayerPrefs.GetFloat("HighScore", 1000).ToString();
+    }
+    public void GetScore()
+    {
+        score = timer.minuter * 60 + timer.sekunder;
+        if(score <PlayerPrefs.GetFloat("Highscore",10000))
+        {           
+            PlayerPrefs.SetFloat("HighScore", score);
+            highscore.text = Mathf.RoundToInt(score).ToString() + ": sekunder";
+        }
+    }
+    public void ResetScore()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
